c68946ad66e231e69b1f2281634e0de1500f2191
Bee <q870214@gmail.com>
2019-07-20 03:12:28 +0800

    春耕0.8 初步完成登入系統(補齊沒上傳的)

---
+++ app/src/main/java/com/boss/vestibularsystemdetection/backsupportdetection/Tool/UserTool/LogoutSystem.java
@@ -0,0 +1,9 @@
+package com.boss.vestibularsystemdetection.backsupportdetection.Tool.UserTool;
+
+public class LogoutSystem {
+
+    public static void logout(){
+        UserManage.setUserEmail("");
+    }
+
+}
